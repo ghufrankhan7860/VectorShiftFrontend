2d04b373363026b1a7fdf2e4f9a1cba425585b64
Ghufran <tabishmahmood445@gmail.com>
2025-06-10 22:22:24 +0530

    submit changes and also fix for textarea variable shows

--- src/submit.js
+++ src/submit.js
@@ -1,10 +1,45 @@
-// submit.js
+// // submit.js
+
+// export const SubmitButton = () => {
+//     return (
+//         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+//             <button type="submit">Submit</button>
+//         </div>
+//     );
+// }
+
+import { useStore } from "./store";
 
 export const SubmitButton = () => {
 
+    const { nodes, edges } = useStore((state) => ({
+        nodes: state.nodes,
+        edges: state.edges,
+    }));
+
+    console.log("Nodes:", nodes);
+    console.log("Edges:", edges);
+    const handleOnClick = () => {
+        try {
+
+
+        } catch (err) {
+            console.log()
+        }
+    }
+
     return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+
             <button type="submit">Submit</button>
+
+
+            <button type="button" onClick={handleOnClick}>Submit</button>
+
         </div>
+
     );
+
 }
