f209652627731df6d7c8ab0b97fddd3fa532fdc4
sonuzero <sonukumarzeroone@gmail.com>
2025-06-10 22:02:44 +0530

    create:axiosClient

---
+++ src/clients/axiosClient.js
@@ -0,0 +1,37 @@
+// axiosClient.js
+import axios from 'axios';
+
+// Create an Axios instance
+const axiosClient = axios.create({
+  baseURL: 'http://127.0.0.1:8000', 
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request Interceptor
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); 
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
--- src/submit.js
+++ src/submit.js
@@ -1,7 +1,5 @@
-// submit.js
 
 export const SubmitButton = () => {
-
     return (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <button type="submit">Submit</button>
