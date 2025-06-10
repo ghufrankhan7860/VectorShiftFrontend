e926867350352c43f7a43b6035313c144c84d5b5
sonuzero <sonukumarzeroone@gmail.com>
2025-06-10 22:54:07 +0530

    create:handleOnSubmit handler for is cyclic computaion

--- src/submit.js
+++ src/submit.js
@@ -1,4 +1,6 @@
+import axiosClient from "./clients/axiosClient";
 import { useStore } from "./store";
+import { v4 as uuidv4 } from "uuid";
 
 export const SubmitButton = () => {
   const { nodes, edges } = useStore((state) => ({
@@ -6,12 +8,26 @@ export const SubmitButton = () => {
     edges: state.edges,
   }));
 
-  console.log("Nodes:", nodes);
-  console.log("Edges:", edges);
-  const handleOnClick = () => {
+  const handleOnClick = async () => {
     try {
+      const idToUUIDMap = new Map();
+      nodes.forEach((node) => idToUUIDMap.set(node.id, uuidv4()));
+      const uuidEdges = edges.map((edge) => [
+        idToUUIDMap.get(edge.source),
+        idToUUIDMap.get(edge.target),
+      ]);
+      const nodesUUIDs = nodes.map((node) => idToUUIDMap.get(node.id));
+
+      const response = await axiosClient.post(`/pipelines/parse`, {
+        pipeline: {
+          nodes: nodesUUIDs,
+          edges: uuidEdges,
+        },
+      });
+
+      console.log(response.data);
     } catch (err) {
-      console.log();
+      console.log(err);
     }
   };
 
@@ -23,9 +39,16 @@ export const SubmitButton = () => {
         justifyContent: "center",
       }}
     >
-      <button type="submit">Submit</button>
-
-      <button type="button" onClick={handleOnClick}>
+      <button
+        type="button"
+        style={{
+          backgroundColor: "#111111",
+          padding: "10px 20px",
+          borderRadius: "10px",
+          color: "#efefef",
+        }}
+        onClick={handleOnClick}
+      >
         Submit
       </button>
     </div>
